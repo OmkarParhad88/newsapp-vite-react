4a2a24f31be4847bc1ae5ae1676ffb7ffb2ca0ab
omkar <omakrparhad7905@gmail.com>
2024-06-16 22:03:40 +0530

    axios

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -79,17 +79,6 @@ const Navbar = ({ country, countryTitle }) => {
        </div>
       </DropdownButton>
      </Dropdown>
-     <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-     >
-      <span className="navbar-toggler-icon"></span>
-     </button>
     </div>
    </nav>
   </>
--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,83 +1,102 @@
-import NewsItem from './NewsItem'
-import { useEffect, useState } from 'react'
-import Spinner from './Spinner'
+import NewsItem from "./NewsItem";
+import { useEffect, useState } from "react";
+import Spinner from "./Spinner";
 import InfiniteScroll from "react-infinite-scroll-component";
+import axios from "axios";
 
-const News = ({ newProgress, key, apiKey, category, country ,size}) => {
-  const [articles, setArticles] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [page, setPage] = useState()
-  const [totalResults, setTotalResults] = useState(0)
+const News = ({ newProgress, key, apiKey, category, country, size }) => {
+ const [articles, setArticles] = useState([]);
+ const [loading, setLoading] = useState(true);
+ const [page, setPage] = useState();
+ const [totalResults, setTotalResults] = useState(0);
+  const [error, setError] = useState(false);
+  const[errorMessage, setErrorMessage] = useState('');
 
-  const capitalizeFirstLetter = (string) => {
-    return string[0].toUpperCase() + string.slice(1);
-  }
+ const capitalizeFirstLetter = (string) => {
+  return string[0].toUpperCase() + string.slice(1);
+ };
  useEffect(() => {
-    updateNews();
-    document.title = `${capitalizeFirstLetter(category)} - News`
-  }, [category,country])
+  updateNews();
+  document.title = `${capitalizeFirstLetter(category)} - News`;
+ }, [category, country]);
 
-  const updateNews = async () => {
-    newProgress(10);
-    const url = `https://newsdata.io/api/1/news?category=${category}&language=en&country=${country}&apikey=${apiKey}&size=${size}`
-    
-    // const url = `https://gnews.io/api/v4/top-headlines?token=${this.props.apiKey}&lang=en&topic=${this.props.category}&page=${this.state.page}&max=${this.props.pageSize}&country=${this.props.country}&q=none`;
-    
-    setLoading(true);
-    let data = await fetch(url);
-
+ const updateNews = async () => {
+  newProgress(10);
+  const url = `https://newsdata.io/api/1/news?category=${category}&language=en&country=${country}&apikey=${apiKey}&size=${size}`;
+  setLoading(true);
+  await axios
+   .get(url)
+   .then((response) => {
     newProgress(30);
-    let preseData = await data.json();
-    console.log(preseData)
+    let data = response.data;
     newProgress(50);
-    setArticles(preseData.results);
-    setPage(preseData.nextPage);
-    setTotalResults(preseData.totalResults);
+    setArticles(data.results);
+    setPage(data.nextPage);
+    setTotalResults(data.totalResults);
     setLoading(false);
     newProgress(100);
-  }
-  
-
-  const fetchMoreData = async () => {
-    const url = `https://newsdata.io/api/1/news?category=${category}&language=en&country=${country}&apikey=${apiKey}&size=${size}&page=${page}`;
-    let data = await fetch(url);
-    let preseData = await data.json();
-    setArticles(articles.concat(preseData.results));
-    setPage(preseData.nextPage)
-    setTotalResults(preseData.totalResults);
+   })
+   .catch((error)=> {
+    console.log(error);
     setLoading(false);
-  }
-    return (
-      <div>
-        <h2 className="text-center" style={{ margin: "86px 0px 18px 0" }}>News- Top Headline From {capitalizeFirstLetter(category)} Category
-        </h2>
-        {loading && <Spinner />}
-        <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length !== totalResults}
-          loader={<Spinner />}>
-          <div className="container">
-            <div className="row">
-              {articles.map(element => {
-                return (
-                  <div className="col-md-4" key={element.link}>
-                    <NewsItem
-                      title={element.title ? element.title.slice(0, 45) : "Title not provided"}
-                      newsUrl={element.link}
-                      imageUrl={element.image_url}
-                      date={element.pubDate}
-                      source={element.source_id}
-                      description={element.description ? element.description.slice(0, 88) : "Description not provided"} />
-                  </div>
-                )
-              })}
-            </div>
-          </div>
-        </InfiniteScroll>
-      </div>
-    )
-  }
+    newProgress(100);
+     setError(true);
+     setErrorMessage(error.message);
+   });
+ };
 
+ const fetchMoreData = async () => {
+  const url = `https://newsdata.io/api/1/news?category=${category}&language=en&country=${country}&apikey=${apiKey}&size=${size}&page=${page}`;
+  let data = await fetch(url);
+  let preseData = await data.json();
+  setArticles(articles.concat(preseData.results));
+  setPage(preseData.nextPage);
+  setTotalResults(preseData.totalResults);
+  setLoading(false);
+ };
+ return (
+  <div>
+   <h2 className="text-center" style={{ margin: "86px 0px 18px 0" }}>
+    News- Top Headline From {capitalizeFirstLetter(category)} Category
+   </h2>
+   {loading && <Spinner />}
+   <InfiniteScroll
+    dataLength={articles.length}
+    next={fetchMoreData}
+    hasMore={articles.length !== totalResults}
+    loader={<Spinner />}
+   >
+    <div className="container">
+     <div className="row">
+      {error ? (
+       <div style={{ color: "red", textAlign: "center" }}>{errorMessage}</div>
+      ) : (
+       articles.map((element) => {
+        return (
+         <div className="col-md-4" key={element.link}>
+          <NewsItem
+           title={
+            element.title ? element.title.slice(0, 45) : "Title not provided"
+           }
+           newsUrl={element.link}
+           imageUrl={element.image_url}
+           date={element.pubDate}
+           source={element.source_id}
+           description={
+            element.description
+             ? element.description.slice(0, 88)
+             : "Description not provided"
+           }
+          />
+         </div>
+        );
+       })
+      )}
+     </div>
+    </div>
+   </InfiniteScroll>
+  </div>
+ );
+};
 
-export default News
+export default News;
