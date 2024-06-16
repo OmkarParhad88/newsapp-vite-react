6e61006d596eb5c79dc730bcb729e01e883e853c
omkar <omakrparhad7905@gmail.com>
2024-06-16 22:48:39 +0530

    path

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = ({ country, countryTitle }) => {
      <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
        <li className="nav-item">
-        <Link className="nav-link" to="/">
+        <Link className="nav-link" to="newsapp">
          Top
         </Link>
        </li>
